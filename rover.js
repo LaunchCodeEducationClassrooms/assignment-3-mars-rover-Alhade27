775db86fd17c56711ceb02ad68a8bb68b7ba04e7
Alex LastName <alexhfamily27@gmail.com>
2021-03-15 04:30:09 +0000

    Finshed building the mars rover

--- rover.js
+++ rover.js
@@ -1,5 +1,76 @@
 class Rover {
-   // Write code here!
+  constructor(position){
+    this.position=position;
+    this.mode="NORMAL";
+    this.generatorWatts=110;
+  }
+
+  recieveMessage(message)
+    {
+      if(message.constructor.name!=="Message")
+        {
+          return false
+        }
+      let name=message.name;
+      let results=[];
+      let canMove;
+      if(this.mode="NORMAL")
+        {
+          canMove=true;
+        } else
+        {
+          canMove=false;
+        }
+
+      if(message.commands!==undefined){
+      for(let command of message.commands){
+        if(command.commandType==="MOVE")
+          {
+            for(let i=0;i<message.commands.length;i++)   //Reloops to see if low power is also a command provided
+              {
+                if(message.commands[i].value==="LOW_POWER")
+                  {
+                    canMove=false;
+                  }
+                  else if (message.commands[i].value==="NORMAL")
+                  {
+                    canMove=true;
+                  }
+                
+              }
+            if(canMove===true)
+              {
+                this.position=command.value;
+                results.push({completed:true});
+              } else {
+                results.push({completed:false});
+              }
+          }
+
+
+        if(command.commandType==="MODE_CHANGE")
+          {
+            if(command.value==="LOW_POWER")
+              {
+                this.mode="LOW_POWER";
+                results.push({completed:true});
+              }
+              else if (command.value==="NORMAL")
+                {
+                  this.mode="NORMAL";
+                  results.push({completed:true});
+                }
+          }
+
+
+        if(command.commandType==="STATUS_CHECK")
+          {
+            results.push({completed:true,roverStatus:{mode:this.mode,generatorWatts:this.generatorWatts,position:this.position}})
+          }
+      }
+      }
+      return {Message:name,results:results}
+    }
 }
 
 module.exports = Rover;
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -7,7 +7,62 @@ const Command = require('../command.js');
 
 
 describe("Rover class", function() {
+  it('constructor sets position and default values for mode and generatorWatts',function(){
+    let rover=new Rover(30);
+    expect(rover.position).toEqual(30);
+    expect(rover.generatorWatts).toEqual(110);
+  });
 
+  it('response returned by receiveMessage contains name of message',function(){
+    let rover=new Rover();
+    let message=new Message("default")
+    expect(rover.recieveMessage(message).Message).toEqual("default");
+  });
+
+  it("response returned by receiveMessage includes two results if two commands are sent in the message", function(){
+    let rover=new Rover();
+    let message=new Message("default","STATUS_CHECK");
+    let i=0;
+    for(items in rover.recieveMessage(message)){i++;}
+    expect(i).toEqual(2);
+  })
+
+  it("responds correctly to status check command",function(){
+      let status=new Command("STATUS_CHECK");
+      let rover = new Rover(10);
+      let message = new Message('hello',[status]);
+      let response = rover.recieveMessage(message);
+      expect(response.results[0].roverStatus).toEqual({mode:"NORMAL",generatorWatts:110,position:10});
+  });
+
+  it("responds correctly to mode change command",function(){
+    let rover=new Rover();
+    let mode1=new Command("MODE_CHANGE","NORMAL");
+    let mode2=new Command("MODE_CHANGE","LOW_POWER");
+    let message1=new Message("normal",[mode1]);
+    let message2=new Message('lowPower',[mode2]);
+    expect(rover.recieveMessage(message1).results).toEqual([{completed:true}]);
+    expect(rover.mode).toEqual(mode1.value);
+    expect(rover.recieveMessage(message2).results).toEqual([{completed:true}]);
+    expect(rover.mode).toEqual(mode2.value);
+  });
+
+  it("responds with false completed value when attempting to move in LOW_POWER mode",function(){
+    let rover = new Rover(1);
+    let move=new Command("MOVE",204932);
+    let lowPower=new Command("MODE_CHANGE","LOW_POWER");
+    let message=new Message("Low power test",[move,lowPower]);
+    expect(rover.recieveMessage(message).results[0]).toEqual({completed:false});
+  });
+
+  it("responds with position for move command",function(){
+    let rover=new Rover(30);
+    let move=new Command("MOVE",650423);
+    let normal=new Command("MODE_CHANGE","NORMAL");
+    let message=new Message("test",[normal,move]);
+    rover.recieveMessage(message);
+    expect(rover.position).toEqual(move.value);
+  });
   // 7 tests here!
 
 });
